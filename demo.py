d8d14dcb779aba6aaa72952dc0187398b3d1558d
Aashutosh Ghule <aashutoshghule01@gmail.com>
2025-07-02 21:19:43 +0530

    test: add type checking to add function

--- demo.py
+++ demo.py
@@ -1,5 +1,8 @@
 def add(a, b):
-    try:
-        return a / b
-    except ZeroDivisionError:
-        return "Cannot divide by zero"
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        try:
+            return a / b
+        except ZeroDivisionError:
+            return "Cannot divide by zero"
+    else:
+        return "Invalid input"
